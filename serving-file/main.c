07f754e6e09ee6a3bd52ca225044e92095da9ab6
amin-baghiyev <baghiyev.amin@gmail.com>
2025-08-10 21:36:22 +0400

    whitespaces fixed

--- serving-file/main.c
+++ serving-file/main.c
@@ -22,25 +22,25 @@ int main(void)
 		"Content-Type: text/html\r\n"
 		"\r\n";
 
-        struct sockaddr_in socket_address, client_address;
-        socket_address.sin_family = AF_INET;
-        socket_address.sin_port = htons(8080);
-        socket_address.sin_addr.s_addr = INADDR_ANY;
-        socklen_t client_address_len = sizeof(client_address);
+	struct sockaddr_in socket_address, client_address;
+	socket_address.sin_family = AF_INET;
+	socket_address.sin_port = htons(8080);
+	socket_address.sin_addr.s_addr = INADDR_ANY;
+	socklen_t client_address_len = sizeof(client_address);
 
-        unsigned short socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-        bind(socket_fd, (struct sockaddr *) &socket_address, sizeof(socket_address));
-        listen(socket_fd, SOMAXCONN);
+	unsigned short socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+	bind(socket_fd, (struct sockaddr *) &socket_address, sizeof(socket_address));
+	listen(socket_fd, SOMAXCONN);
 
 	while (1)
 	{
 		unsigned short client_fd = accept(socket_fd, (struct sockaddr *) &client_address, &client_address_len);
 
 		send(client_fd, http_header, strlen(http_header), 0);
-        	send(client_fd, html_body, html_size, 0);
-        	close(client_fd);
+		send(client_fd, html_body, html_size, 0);
+		close(client_fd);
 	}
 
 	close(socket_fd);
-        return 0;
+	return 0;
 }
